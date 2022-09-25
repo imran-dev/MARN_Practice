bbd99d1bc1ce233fcdb65b2ea88d8172e83788c5
Imran Hossain <imranhossain.dev@gmail.com>
2022-09-25 10:10:04 +0600

    Hello Express JS

---
+++ ExpressJS/PracticeApp01/index.js
@@ -0,0 +1,10 @@
+let express = require('express');
+const app = express();
+
+app.get('/', function (req, res) {
+    res.send('Hello Express JS');
+});
+
+app.listen(8000, function (){
+    console.log('Server Run Success');
+});
