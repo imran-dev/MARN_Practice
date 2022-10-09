c78c05716cc5b00b82c92323afa000fd12129ea1
Imran Hossain <imranhossain.dev@gmail.com>
2022-10-10 01:47:56 +0600

    mongoose crud operations

--- ExpressMyAPI/src/controllers/StudentsController.js
+++ ExpressMyAPI/src/controllers/StudentsController.js
@@ -6,7 +6,44 @@ exports.InsertStudent = (req, res) => {
         if (error) {
             res.status(400).json({status: 'Fail', data: error});
         } else {
-            res.status(201).json({status: 'Success', data: data});
+            res.status(200).json({status: 'Success', data: data});
         }
     });
-}
+};
+
+exports.ReadStudent = (req, res) => {
+    let Query      = {};
+    let Projection = 'Name Roll';
+    StudentsModel.find(Query, Projection, (error, data) => {
+        if (error) {
+            res.status(400).json({status: 'Fail', data: error});
+        } else {
+            res.status(200).json({status: 'Success', data: data});
+        }
+    });
+};
+
+exports.UpdateStudent = (req, res) => {
+    let id      = req.params.id;
+    let Query   = {_id: id};
+    let reqBody = req.body;
+    StudentsModel.updateOne(Query, reqBody, (error, data) => {
+        if (error) {
+            res.status(400).json({status: 'Fail', data: error});
+        } else {
+            res.status(200).json({status: 'Success', data: data});
+        }
+    });
+};
+
+exports.DeleteStudent = (req, res) => {
+    let id      = req.params.id;
+    let Query   = {_id: id};
+    StudentsModel.remove(Query, (error, data) => {
+        if (error) {
+            res.status(400).json({status: 'Fail', data: error});
+        } else {
+            res.status(200).json({status: 'Success', data: data});
+        }
+    });
+};
--- ExpressMyAPI/src/routes/api.js
+++ ExpressMyAPI/src/routes/api.js
@@ -1,15 +1,18 @@
-const express = require('express');
-const HelloController = require("../controllers/HelloController");
+const express            = require('express');
+const router             = express.Router();
+const HelloController    = require("../controllers/HelloController");
 const StudentsController = require("../controllers/StudentsController");
-const router = express.Router();
 
 // This is my first get and post routing
 router.get("/hello-get", HelloController.Hello);
 router.post("/hello-post", HelloController.Hello);
 
+
 // Using Mongoose
 router.post("/InsertStudent", StudentsController.InsertStudent);
-
+router.get("/ReadStudent", StudentsController.ReadStudent);
+router.post("/UpdateStudent/:id", StudentsController.UpdateStudent);
+router.get("/DeleteStudent/:id", StudentsController.DeleteStudent);
 
 
 module.exports = router;
